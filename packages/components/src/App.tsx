d4731649fe030adeb214f4e261b6becfdb17183b
Andrew Hummel <ahummel25@gmail.com>
2020-10-21 22:13:07 -0500

    Latest

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -5,10 +5,10 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  View,
+  View
 } from 'react-native';
 
-import { AppHeader } from './AppHeader'
+import { AppHeader } from './AppHeader';
 
 export function App() {
   return (
@@ -17,7 +17,8 @@ export function App() {
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+          style={styles.scrollView}
+        >
           <AppHeader />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
@@ -26,22 +27,32 @@ export function App() {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Code sharing using Monorepo</Text>
+              <Text style={styles.sectionTitle}>
+                Code sharing using Monorepo!!!!
+              </Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>packages/components/App.tsx</Text> to change this
-                screen and then come back to see your edits (in the phone or the browser).
+                Edit{' '}
+                <Text style={styles.highlight}>
+                  packages/components/App.tsx
+                </Text>{' '}
+                to change this screen and then come back to see your edits (in
+                the phone or the browser).
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Web support via react-native-web</Text>
-              <Text style={styles.sectionDescription}>
-                Run <Text style={styles.highlight}>yarn workspace web start</Text> to 
-                open this app in the browser. 
+              <Text style={styles.sectionTitle}>
+                Web support via react-native-web
               </Text>
               <Text style={styles.sectionDescription}>
-                It will share the same code from mobile, unless you create platform-specific files 
-                using the <Text style={styles.highlight}>.web.tsx</Text> extension 
-                (also supports <Text style={styles.highlight}>.android</Text>,{' '}
+                Run{' '}
+                <Text style={styles.highlight}>yarn workspace web start</Text>{' '}
+                to open this app in the browser.
+              </Text>
+              <Text style={styles.sectionDescription}>
+                It will share the same code from mobile, unless you create
+                platform-specific files using the{' '}
+                <Text style={styles.highlight}>.web.tsx</Text> extension (also
+                supports <Text style={styles.highlight}>.android</Text>,{' '}
                 <Text style={styles.highlight}>.ios</Text>,{' '}
                 <Text style={styles.highlight}>.native</Text>, etc).
               </Text>
@@ -51,36 +62,36 @@ export function App() {
       </SafeAreaView>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: 'white',
+    backgroundColor: 'white'
   },
   engine: {
     position: 'absolute',
-    right: 0,
+    right: 0
   },
   body: {
-    backgroundColor: 'white',
+    backgroundColor: 'white'
   },
   sectionContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    paddingHorizontal: 24
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: 'black',
+    color: 'black'
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
-    color: 'gray',
+    color: 'gray'
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: '700'
   },
   footer: {
     color: 'gray',
@@ -88,8 +99,8 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     padding: 4,
     paddingRight: 12,
-    textAlign: 'right',
-  },
+    textAlign: 'right'
+  }
 });
 
-declare var global: any
+declare var global: any;
